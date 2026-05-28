929c13699906bb573ab150e56711b2a7a58d53c1
vlaneon <vlamae@mail.ru>
2026-05-28 18:43:44 +1000

    Add superuser creation script

--- create_superuser.py
+++ create_superuser.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'ваш_пароль')
+    User.objects.create_superuser('VladaAdmin', 'vlamae@mail.ru', 'vladaadmin')
     print("Суперпользователь создан")
 else:
     print("Суперпользователь уже существует")
