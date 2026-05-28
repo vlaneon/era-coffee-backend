85496e2dca952094972e31642ef0d59d821a67cd
vlaneon <vlamae@mail.ru>
2026-05-28 18:52:29 +1000

    Add superuser creation script for Render

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
 python manage.py shell < create_superuser.py
+python manage.py migrate
