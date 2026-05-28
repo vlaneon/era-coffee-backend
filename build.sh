6f5fa6a3ebe6b175c5cab598f2e6944ed8d34692
vlaneon <vlamae@mail.ru>
2026-05-28 19:07:26 +1000

    Reset admin password to 123

--- build.sh
+++ build.sh
@@ -2,12 +2,4 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py shell << EOF
-from django.contrib.auth import get_user_model;
-User = get_user_model();
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin123');
-    print('Superuser created');
-else:
-    print('Superuser already exists');
-EOF
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', 'admin@example.com', '123')"
