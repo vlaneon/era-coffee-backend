30da940c0891ab14bb695573e1534bcd3b43e391
vlaneon <vlamae@mail.ru>
2026-05-29 00:50:33 +1000

    Add database dump and load it on deploy

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py loaddata db_backup.json
 python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', 'admin@example.com', '123')"
